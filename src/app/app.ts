f1ab1a591ea2b05bac088a2cd953943e94cb4f5e
JoanaLyn P. Galceran <galceranjoana0@gmail.com>
2026-02-04 22:45:01 +0800

    Intial login and dashboardb setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Voting-web-app');
+  protected readonly title = signal('e-ballot');
 }
